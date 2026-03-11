645f9d74bd7e3a57d0d928e65123fd82dc5112b3
salperens <alperen@alperensarikaya.com>
2026-03-11 22:19:22 +0300

    cs fix

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@
 git pull
 docker compose up -d --build
 docker exec couple-balance-app php artisan optimize:clear
+docker exec couple-balance-app php artisan cache:clear
+docker exec couple-balance-app php artisan view:clear
+docker exec couple-balance-app php artisan route:clear
